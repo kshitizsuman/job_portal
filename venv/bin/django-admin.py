7c5bfe6eb6cab0c55a32ea74c67918039577412d
Kshitiz Suman <kshitizsuman@gmail.com>
2021-06-26 17:11:19 +0530

    pushed virtual environment folder

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/kshitiz/Desktop/Puja/code/online_job_portal_project_in_django/venv/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
